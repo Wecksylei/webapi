b8903041a5be40013fc9f83de97446cad88df2c3
Adilio Wecksylei Farias Santos <adilio.santos@softtek.com>
2018-02-08 17:18:36 -0300

    Criando API

---
+++ Controllers/ClienteControlle.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+ 
+namespace  WebAPI.Controllers
+{
+[Route("api/[controller]")]
+public class ClienteController : Controller
+{
+private static List<Cliente> _clientes;
+ 
+static ClienteController()
+{
+_clientes = new List<Cliente>();
+ 
+_clientes.Add(new Cliente()
+{
+Id = 1,
+Nome = "Marcelo Dias"
+});
+ 
+_clientes.Add(new Cliente()
+{
+Id = 2,
+Nome = "Fulano de tal"
+});
+}
+ 
+[HttpGet]
+public IEnumerable<Cliente> GetAll()
+{
+return _clientes.AsReadOnly();
+}
+ 
+[HttpGet("{id}", Name = "GetCliente")]
+public IActionResult GetById(int id)
+{
+var item = _clientes.Find(x => x.Id == id);
+ 
+if (item == null)
+{
+return NotFound();
+}
+ 
+return new ObjectResult(item);
+}
+ 
+[HttpPost]
+public IActionResult Create([FromBody] Cliente item)
+{
+if (item == null)
+{
+return BadRequest();
+}
+ 
+item.Id = (_clientes.Count + 1);
+ 
+_clientes.Add(item);
+ 
+return CreatedAtRoute("GetCliente", new { controller = "Cliente", id = item.Id }, item);
+}
+ 
+[HttpDelete("{id}")]
+public void Delete(int id)
+{
+_clientes.RemoveAll(n => n.Id == id);
+}
+}
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,9 @@
+using System;
+namespace  WebAPI.Models
+{
+public class Cliente
+{
+public int Id { get; set; }
+public string Nome { get; set; }
+}
+}
